268623a4b30b279bba9d095bf5ab056fdb2e84da
Arno Burkhart <arno.burkhart@gmail.com>
2021-10-24 17:11:11 +0200

    #3: Test approach with testcontainers in order to enable system independent CI
    refer to https://www.testcontainers.org/

--- src/test/java/com/baloise/open/sportsrepository/SportsRepositoryApplicationTests.java
+++ src/test/java/com/baloise/open/sportsrepository/SportsRepositoryApplicationTests.java
@@ -1,13 +1,44 @@
 package com.baloise.open.sportsrepository;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
 @SpringBootTest
+@ContextConfiguration(initializers = {SportsRepositoryApplicationTests.Initializer.class})
 class SportsRepositoryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Container
+  public static PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres:11.2")
+      .withDatabaseName("testdb")
+      .withUsername("sa")
+      .withPassword("sa");
+
+  static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+      TestPropertyValues.of(
+          "spring.datasource.url=" + postgres.getJdbcUrl(),
+          "spring.datasource.username=" + postgres.getUsername(),
+          "spring.datasource.password=" + postgres.getPassword(),
+          "spring.flyway.schemas=sports",
+          "spring.jpa.hibernate.naming.physical-strategy=org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl"
+      ).applyTo(configurableApplicationContext.getEnvironment());
+    }
+  }
+
+  @Test
+  void contextLoads() {
+    assertTrue(postgres.isRunning());
+  }
 
 }
